c11941660ec7c2d375bdbb3ed8de55fc345f5c09
W-Sarah <sarahweiss75@gmail.com>
2014-04-16 15:27:21 +0900

    fixed indent after line 43

--- blackjack_game.rb
+++ blackjack_game.rb
@@ -40,73 +40,73 @@ input = menu
 while input != "e"
 
   if input == "p"
-puts "Please enter your name"
-name = gets.chomp
-puts
-puts "Welcome #{name}, let's play Blackjack!"
-puts "----------------"
-
-#initialize game
-deck.shuffle!
-player_cards = []
-dealer_cards = []
-player_cards << deck.pop
-dealer_cards << deck.pop
-player_cards << deck.pop
-dealer_cards << deck.pop
-
-player_total = cards_total(player_cards)
-dealer_total = cards_total(dealer_cards)
-
-puts "#{name}, you have the following cards: #{player_cards} for a total of #{player_total}"
-puts "The dealer has the following cards: #{dealer_cards} for a total of #{dealer_total}"
-
-if player_total == 21
-  puts "Blackjack! Congratulations, you win this game!"
-  else
-
-  #player play
-  puts
-  puts "#{name}, you play first."
-  puts "#{name}, would you like to (h)it or (s)tay?"
-  answer = gets.chomp
-  while ["h","s"].include?(answer) == false
-    puts "Please enter 'h' or 's'"
-    answer = gets.chomp
-  end
-
-  until answer == 's'
-    player_cards << deck.pop
-    player_total = cards_total(player_cards)
-    puts "#{name}, you have the following cards: #{player_cards} for a total of #{player_total}"
-    break if player_total > 20
+    puts "Please enter your name"
+    name = gets.chomp
     puts
-    puts "#{name}, would you like to (h)it or (s)tay?"
-    answer = gets.chomp
-  end
+    puts "Welcome #{name}, let's play Blackjack!"
+    puts "----------------"
 
-  if player_total == 21
-    puts "Blackjack! Congratulations, you win this game!"
-  elsif player_total > 21
-    puts "Sorry #{name}, you have lost this game."
-  else
-    puts "The dealer is going to play now"
-
-  #dealer play
-  until dealer_total > 17
+    #initialize game
+    deck.shuffle!
+    player_cards = []
+    dealer_cards = []
+    player_cards << deck.pop
     dealer_cards << deck.pop
-    dealer_total = cards_total(dealer_cards)
-    puts "The dealer has the following cards: #{dealer_cards} for a total of #{dealer_total}"
-  end
+    player_cards << deck.pop
+    dealer_cards << deck.pop
 
-  #Comparison between the player and the dealer hand
-    if dealer_total > player_total && dealer_total < 22
-      puts "Sorry #{name}, you have lost this game."
-    else
-      puts "Congratulations #{name}, you win this game!"
+    player_total = cards_total(player_cards)
+    dealer_total = cards_total(dealer_cards)
+
+    puts "#{name}, you have the following cards: #{player_cards} for a total of #{player_total}"
+    puts "The dealer has the following cards: #{dealer_cards} for a total of #{dealer_total}"
+
+    if player_total == 21
+      puts "Blackjack! Congratulations, you win this game!"
+      else
+
+      #player play
+      puts
+      puts "#{name}, you play first."
+      puts "#{name}, would you like to (h)it or (s)tay?"
+      answer = gets.chomp
+      while ["h","s"].include?(answer) == false
+        puts "Please enter 'h' or 's'"
+        answer = gets.chomp
+      end
+
+      until answer == 's'
+        player_cards << deck.pop
+        player_total = cards_total(player_cards)
+        puts "#{name}, you have the following cards: #{player_cards} for a total of #{player_total}"
+        break if player_total > 20
+        puts
+        puts "#{name}, would you like to (h)it or (s)tay?"
+        answer = gets.chomp
+      end
+
+      if player_total == 21
+        puts "Blackjack! Congratulations, you win this game!"
+      elsif player_total > 21
+        puts "Sorry #{name}, you have lost this game."
+      else
+        puts "The dealer is going to play now"
+
+      #dealer play
+      until dealer_total > 17
+        dealer_cards << deck.pop
+        dealer_total = cards_total(dealer_cards)
+        puts "The dealer has the following cards: #{dealer_cards} for a total of #{dealer_total}"
+      end
+
+      #Comparison between the player and the dealer hand
+        if dealer_total > player_total && dealer_total < 22
+          puts "Sorry #{name}, you have lost this game."
+        else
+          puts "Congratulations #{name}, you win this game!"
+        end
+      end
     end
-  end
-end
 
   else 
     puts "Invalid - Please enter (p) or (e)"
